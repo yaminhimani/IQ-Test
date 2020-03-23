c44e15e20865752dcf0291d2555c51b89f7b2969
yaminhimani <yaminhimani7@outlook.com>
2020-03-23 15:28:26 -0400

    added a new class and started working on constructor

---
+++ src/Game.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Game {
+
+    private ArrayList<Questions> questions;
+    public Game(){
+        questions= new ArrayList<Questions>(); //creating new array list of questions
+        String question="What is 20 percent of 30 Dollars?";
+        String[] answers= {" 5 dollars "," 6 dollars "," 4 dollars ", " 8 dollars "};
+
+
+
+
+
+    }
+
+
+}
