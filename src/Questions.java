0d96c5cfd8a9bdfce04c4f5f8eeeec683f2f4a03
yaminhimani <yaminhimani7@outlook.com>
2020-03-23 14:58:37 -0400

    did the constructor and getters

--- src/Questions.java
+++ src/Questions.java
@@ -1,8 +1,32 @@
+import java.util.ArrayList;
+
 public class Questions {
 
+    private String question;
+    private ArrayList<String> answerChoicesList;
+    private String answer;
+    public Questions(String question, String[] answerChoices, String answer){
+
+            this.question=question;
+            this.answerChoicesList= new ArrayList<String>();
+            for(int i=0;i< answerChoices.length;i++){                          //this loop takes the answer choices and puts it in the array list
+                this.answerChoicesList.add(answerChoices[i]);
+            }
+    }
+
+    public String getQuestion() {
+        return question;
+    }
 
 
 
+    public ArrayList<String> getAnswerChoicesList() {
+        return answerChoicesList;
+    }
 
+
+    public String getAnswer() {
+        return answer;
+    }
 
 }
